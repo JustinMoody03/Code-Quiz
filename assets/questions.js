08de65bc11e017074f5427612882585d5b730473
Justin Moody <Justindmoody@yahoo.com>
2022-09-15 15:16:14 -0400

    Coding Quiz Complete

---
+++ assets/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "When was JavaScript invented?",
+        answer: "1995",
+        options: [
+            "2008",
+            "2015",
+            "1995",
+            "1990"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Whats the current version of HTML?",
+        answer: "5",
+        options: [
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which is a coding language?",
+        answer: "Python",
+        options: [
+            "Python",
+            "Jawascript",
+            "Cloud",
+            "Terminal"
+        ]
+    },
+];
